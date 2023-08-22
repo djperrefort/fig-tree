7248290303ad9d3da2151cf78ed0e318781bdbf3
Daniel <djperrefort@gmail.com>
2023-08-22 19:46:59 -0400

    Adds model managers for signup app

---
+++ fig_tree/apps/signup/managers.py
@@ -0,0 +1,63 @@
+"""Database model managers.
+
+The ``managers`` module defines custom model managers for encapsulating common
+query logic. Managers can be attached to database models to extend a model's
+default querying capabilities and to facilitate common data retrieval tasks.
+ Model objects can be found in the ``models`` module.
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.base_user import BaseUserManager
+from django.core.validators import validate_email
+
+if TYPE_CHECKING:  # Protect against circular import
+    from .models import AuthUser
+
+
+class AuthUserManager(BaseUserManager):
+    """Custom user model manager"""
+
+    @staticmethod
+    def create_user(username: str, password: str, email: str, **extra_fields) -> AuthUser:
+        """Create and save a new user with the given email and password"""
+
+        validate_email(email)
+
+        user_model = get_user_model()
+        user = user_model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    @classmethod
+    def create_staff_user(cls, username: str, password: str, email: str, **extra_fields) -> AuthUser:
+        """Convenience function for creating a new staff user"""
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have ``is_staff`` set to ``True```.')
+
+        return cls.create_user(username, password, email, **extra_fields)
+
+    @classmethod
+    def create_superuser(cls, username: str, password: str, email: str, **extra_fields) -> AuthUser:
+        """Convenience function for creating a new superuser"""
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have ``is_staff`` set to ``True```.')
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have ``is_superuser`` set to ``True``.')
+
+        return cls.create_user(username, password, email, **extra_fields)
