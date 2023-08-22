45dbb4547a72c97fb12f6dbbfb953cb30666bc7c
Daniel <djperrefort@gmail.com>
2023-08-22 19:00:49 -0400

    Define path names with hyphens

--- fig_tree/apps/data_api/urls.py
+++ fig_tree/apps/data_api/urls.py
@@ -10,57 +10,57 @@ URL Routing Configuration
 +-------------------------+--------------------------+------------------------+
 | URL                     | View                     | Name                   |
 +=========================+==========================+========================+
-| ``address``             | ``AddressListView``      | ``address_list``       |
+| ``address``             | ``AddressListView``      | ``address-list``       |
 +-------------------------+--------------------------+------------------------+
-| ``address/<str:id>``    | ``AddressDetailView``    | ``address_detail``     |
+| ``address/<str:id>``    | ``AddressDetailView``    | ``address-detail``     |
 +-------------------------+--------------------------+------------------------+
-| ``citation``            | ``CitationListView``     | ``citation_list``      |
+| ``citation``            | ``CitationListView``     | ``citation-list``      |
 +-------------------------+--------------------------+------------------------+
-| ``citation/<str:id>``   | ``CitationDetailView``   | ``citation_detail``    |
+| ``citation/<str:id>``   | ``CitationDetailView``   | ``citation-detail``    |
 +-------------------------+--------------------------+------------------------+
-| ``event``               | ``EventListView``        | ``event_list``         |
+| ``event``               | ``EventListView``        | ``event-list``         |
 +-------------------------+--------------------------+------------------------+
-| ``event/<str:id>``      | ``EventDetailView``      | ``event_detail``       |
+| ``event/<str:id>``      | ``EventDetailView``      | ``event-detail``       |
 +-------------------------+--------------------------+------------------------+
-| ``family``              | ``FamilyListView``       | ``family_list``        |
+| ``family``              | ``FamilyListView``       | ``family-list``        |
 +-------------------------+--------------------------+------------------------+
-| ``family/<str:id>``     | ``FamilyDetailView``     | ``family_detail``      |
+| ``family/<str:id>``     | ``FamilyDetailView``     | ``family-detail``      |
 +-------------------------+--------------------------+------------------------+
-| ``media``               | ``MediaListView``        | ``media_list``         |
+| ``media``               | ``MediaListView``        | ``media-list``         |
 +-------------------------+--------------------------+------------------------+
-| ``media/<str:id>``      | ``MediaDetailView``      | ``media_detail``       |
+| ``media/<str:id>``      | ``MediaDetailView``      | ``media-detail``       |
 +-------------------------+--------------------------+------------------------+
-| ``name``                | ``NameListView``         | ``name_list``          |
+| ``name``                | ``NameListView``         | ``name-list``          |
 +-------------------------+--------------------------+------------------------+
-| ``name/<str:id>``       | ``NameDetailView``       | ``name_detail``        |
+| ``name/<str:id>``       | ``NameDetailView``       | ``name-detail``        |
 +-------------------------+--------------------------+------------------------+
-| ``note``                | ``NoteListView``         | ``note_list``          |
+| ``note``                | ``NoteListView``         | ``note-list``          |
 +-------------------------+--------------------------+------------------------+
-| ``note/<str:id>``       | ``NoteDetailView``       | ``note_detail``        |
+| ``note/<str:id>``       | ``NoteDetailView``       | ``note-detail``        |
 +-------------------------+--------------------------+------------------------+
-| ``person``              | ``PersonListView``       | ``person_list``        |
+| ``person``              | ``PersonListView``       | ``person-list``        |
 +-------------------------+--------------------------+------------------------+
-| ``person/<str:id>``     | ``PersonDetailView``     | ``person_detail``      |
+| ``person/<str:id>``     | ``PersonDetailView``     | ``person-detail``      |
 +-------------------------+--------------------------+------------------------+
-| ``place``               | ``PlaceListView``        | ``place_list``         |
+| ``place``               | ``PlaceListView``        | ``place-list``         |
 +-------------------------+--------------------------+------------------------+
-| ``place/<str:id>``      | ``PlaceDetailView``      | ``place_detail``       |
+| ``place/<str:id>``      | ``PlaceDetailView``      | ``place-detail``       |
 +-------------------------+--------------------------+------------------------+
-| ``repository``          | ``RepositoryListView``   | ``repository_list``    |
+| ``repository``          | ``RepositoryListView``   | ``repository-list``    |
 +-------------------------+--------------------------+------------------------+
-| ``repository/<str:id>`` | ``RepositoryDetailView`` | ``repository_detail``  |
+| ``repository/<str:id>`` | ``RepositoryDetailView`` | ``repository-detail``  |
 +-------------------------+--------------------------+------------------------+
-| ``source``              | ``SourceListView``       | ``source_list``        |
+| ``source``              | ``SourceListView``       | ``source-list``        |
 +-------------------------+--------------------------+------------------------+
-| ``source/<str:id>``     | ``SourceDetailView``     | ``source_detail``      |
+| ``source/<str:id>``     | ``SourceDetailView``     | ``source-detail``      |
 +-------------------------+--------------------------+------------------------+
-| ``tag``                 | ``TagListView``          | ``tag_list``           |
+| ``tag``                 | ``TagListView``          | ``tag-list``           |
 +-------------------------+--------------------------+------------------------+
-| ``tag/<str:id>``        | ``TagDetailView``        | ``tag_detail``         |
+| ``tag/<str:id>``        | ``TagDetailView``        | ``tag-detail``         |
 +-------------------------+--------------------------+------------------------+
-| ``url``                 | ``URLListView``          | ``url_list``           |
+| ``url``                 | ``URLListView``          | ``url-list``           |
 +-------------------------+--------------------------+------------------------+
-| ``url/<str:id>``        | ``URLDetailView``        | ``url_detail``         |
+| ``url/<str:id>``        | ``URLDetailView``        | ``url-detail``         |
 +-------------------------+--------------------------+------------------------+
 """
 
@@ -71,30 +71,30 @@ from . import views
 app_name = 'data_api'
 
 urlpatterns = [
-    path('address', views.AddressListView.as_view(), name='address_list'),
-    path('address/<str:id>', views.AddressDetailView.as_view(), name='address_detail'),
-    path('citation', views.CitationListView.as_view(), name='citation_list'),
-    path('citation/<str:id>', views.CitationDetailView.as_view(), name='citation_detail'),
-    path('event', views.EventListView.as_view(), name='event_list'),
-    path('event/<str:id>', views.EventDetailView.as_view(), name='event_detail'),
-    path('family', views.FamilyListView.as_view(), name='family_list'),
-    path('family/<str:id>', views.FamilyDetailView.as_view(), name='family_detail'),
-    path('media', views.MediaListView.as_view(), name='media_list'),
-    path('media/<str:id>', views.MediaDetailView.as_view(), name='media_detail'),
-    path('name', views.NameListView.as_view(), name='name_list'),
-    path('name/<str:id>', views.NameDetailView.as_view(), name='name_detail'),
-    path('note', views.NoteListView.as_view(), name='note_list'),
-    path('note/<str:id>', views.NoteDetailView.as_view(), name='note_detail'),
-    path('person', views.PersonListView.as_view(), name='person_list'),
-    path('person/<str:id>', views.PersonDetailView.as_view(), name='person_detail'),
-    path('place', views.PlaceListView.as_view(), name='place_list'),
-    path('place/<str:id>', views.PlaceDetailView.as_view(), name='place_detail'),
-    path('repository', views.RepositoryListView.as_view(), name='repository_list'),
-    path('repository/<str:id>', views.RepositoryDetailView.as_view(), name='repository_detail'),
-    path('source', views.SourceListView.as_view(), name='source_list'),
-    path('source/<str:id>', views.SourceDetailView.as_view(), name='source_detail'),
-    path('tag', views.TagListView.as_view(), name='tag_list'),
-    path('tag/<str:id>', views.TagDetailView.as_view(), name='tag_detail'),
-    path('url', views.URLListView.as_view(), name='url_list'),
-    path('url/<str:id>', views.URLDetailView.as_view(), name='url_detail'),
+    path('address', views.AddressListView.as_view(), name='address-list'),
+    path('address/<str:id>', views.AddressDetailView.as_view(), name='address-detail'),
+    path('citation', views.CitationListView.as_view(), name='citation-list'),
+    path('citation/<str:id>', views.CitationDetailView.as_view(), name='citation-detail'),
+    path('event', views.EventListView.as_view(), name='event-list'),
+    path('event/<str:id>', views.EventDetailView.as_view(), name='event-detail'),
+    path('family', views.FamilyListView.as_view(), name='family-list'),
+    path('family/<str:id>', views.FamilyDetailView.as_view(), name='family-detail'),
+    path('media', views.MediaListView.as_view(), name='media-list'),
+    path('media/<str:id>', views.MediaDetailView.as_view(), name='media-detail'),
+    path('name', views.NameListView.as_view(), name='name-list'),
+    path('name/<str:id>', views.NameDetailView.as_view(), name='name-detail'),
+    path('note', views.NoteListView.as_view(), name='note-list'),
+    path('note/<str:id>', views.NoteDetailView.as_view(), name='note-detail'),
+    path('person', views.PersonListView.as_view(), name='person-list'),
+    path('person/<str:id>', views.PersonDetailView.as_view(), name='person-detail'),
+    path('place', views.PlaceListView.as_view(), name='place-list'),
+    path('place/<str:id>', views.PlaceDetailView.as_view(), name='place-detail'),
+    path('repository', views.RepositoryListView.as_view(), name='repository-list'),
+    path('repository/<str:id>', views.RepositoryDetailView.as_view(), name='repository-detail'),
+    path('source', views.SourceListView.as_view(), name='source-list'),
+    path('source/<str:id>', views.SourceDetailView.as_view(), name='source-detail'),
+    path('tag', views.TagListView.as_view(), name='tag-list'),
+    path('tag/<str:id>', views.TagDetailView.as_view(), name='tag-detail'),
+    path('url', views.URLListView.as_view(), name='url-list'),
+    path('url/<str:id>', views.URLDetailView.as_view(), name='url-detail'),
 ]
