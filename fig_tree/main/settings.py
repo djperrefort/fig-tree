abcb17666471da457e5f3081861a094f25d3767f
Daniel <djperrefort@gmail.com>
2023-09-03 13:36:12 -0400

    Makes DB settings configurable via env

--- fig_tree/main/settings.py
+++ fig_tree/main/settings.py
@@ -83,10 +83,15 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+_driver = os.environ.get('DB_DRIVER', 'sqlite3')
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": f'django.db.backends.{_driver}',
+        "NAME": os.environ.get('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        "USER": os.environ.get('DB_USER', ''),
+        "PASSWORD": os.environ.get('DB_PASSWORD', ''),
+        "HOST": os.environ.get('DB_HOST', ''),
+        "PORT": os.environ.get('DB_PORT', ''),
     }
 }
 
