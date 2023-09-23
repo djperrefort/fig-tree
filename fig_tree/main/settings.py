5769a14ce30ec4e0e84f3337b76f16700edb7fef
Daniel Perrefort <djperrefort@gmail.com>
2023-09-22 23:23:47 -0400

    Adds 127.0.0.1 to default ALLOWED_HOSTS

--- fig_tree/main/settings.py
+++ fig_tree/main/settings.py
@@ -17,7 +17,7 @@ load_dotenv()
 # Security and authentication settings
 DEBUG = os.environ.get('DEBUG', default='0') != '0'
 SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", default="localhost").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", default="localhost 127.0.0.1").split(" ")
 
 # If running in debug mode, save emails to disk instead of sending them
 if DEBUG:
