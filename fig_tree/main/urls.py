2da650416c5f6aac4a36f771dc8834aafdc9d60e
Daniel Perrefort <djperrefort@gmail.com>
2023-09-23 14:17:29 -0400

    Adds error page urls to test against

--- fig_tree/main/urls.py
+++ fig_tree/main/urls.py
@@ -1,7 +1,8 @@
 """
 The `main.urls` module defines the primary website URL configuration.
 The table below outlines routing patterns different URL endpoints.
-Further traffic routing is handled internally by each application to route specific URLs to individual pages.
+Further traffic routing is handled internally by each application to
+route specific URLs to individual pages.
 
 | URL Pattern  | Application                  | Namespace       |
 |--------------|------------------------------|-----------------|
@@ -10,15 +11,25 @@ Further traffic routing is handled internally by each application to route speci
 | ``api/``     | ``apps.gen_rest_api``        | ``gen_rest_api``|
 | ``signup/``  | ``apps.signup``              | ``signup``      |
 
+The following pages are also added when running in ``DEBUG`` mode to support
+testing and development.
+
+| URL Pattern   | Description                    | Registered Name |
+|---------------|--------------------------------|-----------------|
+| ``tests/400`` | Renders a 400 HTTP error code. | ``test-400``    |
+| ``tests/403`` | Renders a 403 HTTP error code. | ``test-403``    |
+| ``tests/404`` | Renders a 404 HTTP error code. | ``test-404``    |
+| ``tests/500`` | Renders a 500 HTTP error code. | ``test-500``    |
 """
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
 from apps.error_pages import handlers
 
-# Add handlers for different error codes as desired
+# See the ``error_pages`` app for more information on error code handlers
 handler400 = handlers.handler400
 handler403 = handlers.handler403
 handler404 = handlers.handler404
@@ -31,3 +42,12 @@ urlpatterns = [
     path('api/', include('apps.gen_rest_api.urls', namespace='gen_rest_api')),
     path('signup/', include('apps.signup.urls', namespace='signup')),
 ]
+
+# Add dedicated error pages for testing purposes
+if settings.DEBUG:
+    urlpatterns += [
+        path('tests/400', handler400, name='test-400'),
+        path('tests/403', handler403, name='test-403'),
+        path('tests/404', handler404, name='test-404'),
+        path('tests/500', handler500, name='test-500'),
+    ]
