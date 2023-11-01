ec12e2e17277097188a82de2beedcae2ee1b5ac2
Daniel Perrefort <djperrefort@gmail.com>
2023-10-31 20:57:02 -0400

    Adds missing docstring

--- fig_tree/apps/gen_rest_api/admin.py
+++ fig_tree/apps/gen_rest_api/admin.py
@@ -1,17 +1,24 @@
+"""
+The `admin` module defines custom administrative interfaces used by the
+website admin portal. Admin classes are used to extend and enhance the
+management of application settings by customizing the appearance, functionality,
+and permissions of admin portal interfaces.
+"""
+
 from django.contrib import admin
 
-from . import models
+from .models import *
 
 
 class TreePermissionInline(admin.TabularInline):
     """Inline admin element for family tree user permissions"""
 
-    model = models.TreePermission
+    model = TreePermission
     show_change_link = False
     extra = 0
 
 
-@admin.register(models.Tree)
+@admin.register(Tree)
 class TreeAdmin(admin.ModelAdmin):
     """Admin interface for family tree objects"""
 
