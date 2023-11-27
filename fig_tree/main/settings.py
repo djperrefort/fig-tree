9e502f5c05d25cac9c7bcfa001edd882ceeffb97
Daniel Perrefort <djperrefort@gmail.com>
2023-11-26 21:06:02 -0500

    Switch to django-environ

--- fig_tree/main/settings.py
+++ fig_tree/main/settings.py
@@ -1,28 +1,30 @@
 """Top level Django application settings."""
 
-import os
 import sys
 from datetime import timedelta
 from pathlib import Path
 
+import environ
 from django.core.management.utils import get_random_secret_key
-from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Setup runtime environment
+
+env = environ.Env()
 sys.path.insert(0, str(BASE_DIR))
-load_dotenv()
 
 # Security and authentication settings
-DEBUG = os.environ.get('DEBUG', default='0') != '0'
-SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", default="localhost 127.0.0.1").split(" ")
+
+DEBUG = env.bool('DEBUG', False)
+SECRET_KEY = env.str('SECRET_KEY', get_random_secret_key())
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ["localhost", "127.0.0.1"])
 
 # If running in debug mode, save emails to disk instead of sending them
+
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-    EMAIL_FILE_PATH = Path(os.environ.get('EMAIL_FILE_PATH', BASE_DIR.parent / 'email'))
+    EMAIL_FILE_PATH = env.path('EMAIL_FILE_PATH', BASE_DIR.parent / 'email')
 
 # Application Configuration
 
@@ -109,24 +111,14 @@ REST_FRAMEWORK = {
 }
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
-_driver = os.environ.get('DB_DRIVER', 'sqlite3')
-_name = BASE_DIR / 'fig_tree.sqlite3' if _driver == 'sqlite3' else 'fig_tree'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_DB_PATH = BASE_DIR / 'keystone.db'
 DATABASES = {
-    'default': {
-        "ENGINE": f'django.db.backends.{_driver}',
-        "NAME": os.environ.get('DB_NAME', _name),
-        "USER": os.environ.get('DB_USER', ''),
-        "PASSWORD": os.environ.get('DB_PASSWORD', ''),
-        "HOST": os.environ.get('DB_HOST', 'localhost'),
-        "PORT": os.environ.get('DB_PORT', '5432'),
-    }
+    'default': env.db('DATABASE_URL', f'sqlite:///{DEFAULT_DB_PATH}')
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
@@ -139,7 +131,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
@@ -147,13 +138,6 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = os.environ.get('STATIC_URL', 'static/')
-STATIC_ROOT = Path(os.environ.get('STATIC_ROOT', Path.cwd() / 'static_root'))
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = env.str('STATIC_URL', 'static/')
+STATIC_ROOT = env.path('STATIC_ROOT', BASE_DIR / 'static_root')
